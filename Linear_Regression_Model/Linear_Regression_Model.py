5acc6b959ee6271f70f3412133f830bb3090af31
Kunal <kunal2000goel@gmail.com>
2026-04-27 21:30:53 +0530

    Corrected formulaes

--- Linear_Regression_Model/Linear_Regression_Model.py
+++ Linear_Regression_Model/Linear_Regression_Model.py
@@ -2,15 +2,15 @@
 # Can make Linear, Quadratic, Cubit, etc lines
 
 # Initial weights (Define the curve type)
-W = [20,0.2]
+W = [0.1,0.2,0.1]
 # Learning Rate
-a = 0.001
+a = 0.0002
 # Stopping criteria
 E = 0.001
 
 # Data
-# data = [[1,1],[2,4],[3,9],[4,16],[5,25],[6,36],[7,49],[8,64],[9,81],[10,100]]
-data = [[1,2],[2,4],[3,6],[4,8],[5,10],[6,12]]
+data = [[1,1],[2,4],[3,9],[4,16],[5,25],[6,36],[7,49],[8,64],[9,81],[10,100]]
+# data = [[1,2],[2,4],[3,6],[4,8],[5,10],[6,12]]
 def GradientDescent(W):
     Guess = []
     for j in range(0,len(data),1):
@@ -25,14 +25,15 @@ def GradientDescent(W):
         Cost = Cost + ((data[i][1] - Guess[i])**2)/len(data)
     print(Cost)
 
+
     Grad = [0]*len(W)
     for i in range(0,len(Grad),1):
         for j in range (0,len(data),1):
-            Grad[i] = Grad[i] + ((data[j][1] - Guess[j])*data[j][0]**i)/len(data)**i
+            Grad[i] = Grad[i] - ((data[j][1] - Guess[j])*data[j][0]**i)/len(data)
     print(Grad)
 
     for i in range (0,len(W),1):
-        W[i] = W[i] + a*Grad[i]
+        W[i] = W[i] - a*Grad[i]
     return [W, Cost]
 
 Cost = 1
@@ -54,7 +55,7 @@ print(f"mean = {mean}")
 
 SST = 0
 for i in data:
-    SST = SST + (i[0] - mean)**2
+    SST = SST + (i[1] - mean)**2
 
 SSE = Cost*len(data)
 print(f"SSE = {SSE}")
