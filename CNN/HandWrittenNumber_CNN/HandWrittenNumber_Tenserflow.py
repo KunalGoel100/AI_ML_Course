f9c21c3d8f9f8050091af1b90dc815210fc1d0d1
Kunal <kunal2000goel@gmail.com>
2026-09-14 17:46:40 +0530

    Better Handwritten number CNN

--- CNN/HandWrittenNumber_CNN/HandWrittenNumber_Tenserflow.py
+++ CNN/HandWrittenNumber_CNN/HandWrittenNumber_Tenserflow.py
@@ -3,29 +3,29 @@ import numpy as np
 
 import cv2
 
-# Read image in grayscale
-x_img = cv2.imread(
-    r"C:\KunalGoel\AI_ML_Course\CNN\HandWrittenNumber_CNN\Data\Eight_2.jpg",
-    cv2.IMREAD_GRAYSCALE
-)
-# Resize to 28x28
-x_img = cv2.resize(x_img, (28, 28))
-
-# Normalize pixel values
-x_img = x_img / 255.0
-
-# Reshape for CNN
-x_img = x_img.reshape(1, 28, 28, 1)
-x_img = 1-x_img
+# # Read image in grayscale
+# x_img = cv2.imread(
+#     r"C:\KunalGoel\AI_ML_Course\CNN\HandWrittenNumber_CNN\Data\Two_2.jpg",
+#     cv2.IMREAD_GRAYSCALE
+# )
+# # Resize to 28x28
+# x_img = cv2.resize(x_img, (28, 28))
 #
+# # Normalize pixel values
+# x_img = x_img / 255.0
+#
+# # Reshape for CNN
+# x_img = x_img.reshape(1, 28, 28, 1)
+# x_img = 1-x_img
+# #
 # ####################################################
-# # Visualise the image
-import matplotlib.pyplot as plt
-#
-plt.imshow(x_img[0], cmap='gray')
-# plt.title(f"Label: {y_test[3]}")
-plt.axis('off')   # removes axis numbers
-plt.show()
+# # # Visualise the image
+# import matplotlib.pyplot as plt
+# #
+# plt.imshow(x_img[0], cmap='gray')
+# # plt.title(f"Label: {y_test[3]}")
+# plt.axis('off')   # removes axis numbers
+# plt.show()
 
 ###################################################
 # # Load dataset (MNIST)
@@ -76,16 +76,66 @@ model.compile(
 # model.fit(x_train, y_train, epochs=50, validation_data=(x_test, y_test))
 # model.save_weights("SavedWeights.weights.h5")
 # Test prediction
+###############################################################
+img = cv2.imread(r"C:\KunalGoel\AI_ML_Course\CNN\HandWrittenNumber_CNN\Data\Exp_1.jpg", 0)
 
-pred = model.predict(x_img)
-print(pred)
-print("Prediction:", np.argmax(pred))
-print("Confidence:", np.max(pred))
-Choice = input("Is the prediction Correct: Y , N")
-if Choice == "Y" or Choice == "y":
-    model.fit(x_img,np.array([np.argmax(pred)]), epochs=1)
-    model.save_weights("SavedWeights.weights.h5")
-else:
-    correct = int(input("Enter correct number: "))
-    model.fit(x_img, np.array([correct]), epochs=1)
-    model.save_weights("SavedWeights.weights.h5")
+_, thresh = cv2.threshold(
+    img,
+    127,
+    255,
+    cv2.THRESH_BINARY_INV
+)
+contours, _ = cv2.findContours(
+    thresh,
+    cv2.RETR_EXTERNAL,
+    cv2.CHAIN_APPROX_SIMPLE
+)
+boxes = []
+for cnt in contours:
+    x,y,w,h = cv2.boundingRect(cnt)
+    if w > 8 and h > 8:
+        boxes.append((x,y,w,h))
+boxes = sorted(boxes,key=lambda b:b[0])
+# print(boxes)
+expression = ""
+for x,y,w,h in boxes:
+    roi = thresh[y:y+h, x:x+w]
+    # Keep aspect ratio
+    scale = 20.0 / max(w, h)
+    new_w = int(w * scale)
+    new_h = int(h * scale)
+    roi = cv2.resize(roi,(new_w,new_h))
+    canvas = np.zeros((28, 28), dtype=np.uint8)
+    x_offset = (28 - new_w) // 2
+    y_offset = (28 - new_h) // 2
+    canvas[y_offset:y_offset + new_h,x_offset: x_offset + new_w] = roi
+    roi = canvas.astype("float32") / 255.0
+    roi = roi.reshape(1, 28, 28, 1)
+    # # Visualise the image
+    import matplotlib.pyplot as plt
+
+    plt.imshow(roi[0], cmap='gray')
+    # plt.title(f"Label: {y_test[3]}")
+    plt.axis('off')  # removes axis numbers
+    plt.show()
+###############################################################
+    pred = model.predict(roi)
+    # print(pred)
+    print("Prediction:", np.argmax(pred))
+    print("Confidence:", np.max(pred))
+    Choice = input("Is the prediction Correct: Y , N")
+    if Choice == "Y" or Choice == "y":
+        # model.fit(x_img,np.array([np.argmax(pred)]), epochs=1)
+        # model.save_weights("SavedWeights.weights.h5")
+        # print("Weights Updated")
+        expression += str(np.argmax(pred))
+    elif Choice == "N" or Choice == "n":
+        correct = int(input("Enter correct number: "))
+        model.fit(roi, np.array([correct]), epochs=3)
+        model.save_weights("SavedWeights.weights.h5")
+        print("Weights Updated")
+        expression += str(correct)
+    else:
+        pass
+print(f"Expression: {expression}")
+
